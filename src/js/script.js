d0c8ae446c6c75e3d0c62364ac5561631205d569
GABRIEL DE SOUZA FURTADO <97768716+GabrielSF2022@users.noreply.github.com>
2023-01-03 13:15:49 -0300

    final commit

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,8 @@ window.onscroll = () => {
 }
 
 
+
+// SHOW SCROLL UP
 const scrollHeader = () =>{
     const header = document.getElementById('header')
     this.scrollY >=50 ? header.classList.add('bg-header')
@@ -21,9 +23,24 @@ window.addEventListener('scroll', scrollHeader);
 
 
 
+// NAVBAR ACTIVE
 document.querySelectorAll('.navbar li a').forEach(el => {
     el.onclick = () => {
       document.querySelectorAll('.navbar li a').forEach(el => el.classList.remove('active'));
       el.classList.add('active');
     }
-  })
+})
+
+
+// SCROLL REVEAL
+const sr = ScrollReveal({
+    origin:'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+})
+
+sr.reveal(`.content-1, .numbers-section, .clients`)
+sr.reveal(`.content-2, .info1, .card-footer`, {origin:'bottom'})
+sr.reveal(`.product-image, .container-info2, .apps`, {origin:'left'})
+sr.reveal(`.product-text, .info2-image`, {origin:'right'})
